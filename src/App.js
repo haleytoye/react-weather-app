c82de64ff0310354d4ddda1efd89f804262ed8fe
haleytoye <hjtoye@gmail.com>
2023-03-06 16:26:00 -0500

    Added Bootstrap and container to App.js

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.shecodes.io/graduates/53070-haley-toye"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Haley Toye{" "}
-        </a>
-        and is{" "}
-        <a
-          href="https://github.com/haleytoye/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.shecodes.io/graduates/53070-haley-toye"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Haley Toye{" "}
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/haleytoye/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
