bb678e703c31c39a1a3043df1a1cb1f3424ee30e
haleytoye <hjtoye@gmail.com>
2023-03-06 13:33:13 -0500

    Added footer with GitHub link

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather</h1>
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.shecodes.io/graduates/53070-haley-toye"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Haley Toye{" "}
+        </a>
+        and is{" "}
+        <a
+          href="https://github.com/haleytoye/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
