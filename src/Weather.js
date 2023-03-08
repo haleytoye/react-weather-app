ad6429f7d2d05d647840bcc7b7a6e7b97a0c72c3
haleytoye <hjtoye@gmail.com>
2023-03-08 10:59:45 -0500

    Created setWeather and FormattedDate

--- src/Weather.js
+++ src/Weather.js
@@ -1,62 +1,78 @@
-import React from "react";
-import "./Weather.css";
+import React, { useState } from "react";
 import axios from "axios";
+import FormattedDate from "./FormattedDate";
+import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
   function handleResponse(response) {
-    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      city: response.data.city,
+      date: new Date(response.data.time * 1000),
+      description: response.data.condition.description,
+      temperature: Math.round(response.data.temperature.current),
+      feels: Math.round(response.data.temperature.feels_like),
+      wind: Math.round(response.data.wind.speed),
+      humidity: response.data.temperature.humidity,
+      icon: response.data.condition.icon_url,
+    });
   }
-  const apiKey = "1o0f4aea9af54436031a3t4f3b2ca21f";
-  let city = "Lisbon";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-  let units = "metric";
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-6">
-            <input
-              type="search"
-              placeholder="Enter a city.."
-              className="from-control w-100"
-              autoFocus="on"
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className=" btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
-      <h1>Toronto</h1>
-      <ul>
-        <li>Wednesday 7:15</li>
-        <li>Mostly Sunny</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-          <div className="d-flex">
-            <img
-              src="https://weather.gc.ca/weathericons/small/00.png"
-              alt="can-sun"
-            />
 
-            <span className="temperature">6</span>
-            <span className="weather-temperature-units"> °C</span>
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-6">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="from-control w-100"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className=" btn btn-primary w-100"
+              />
+            </div>
           </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: %</li>
-            <li>Humidity: %</li>
-            <li>Wind: km/h</li>
-          </ul>
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>
+            <FormattedDate date={weatherData.date} />
+          </li>
+          <li className="text-capitalize">{weatherData.description}</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            <div className="d-flex">
+              <img src={weatherData.icon} alt={weatherData.icon} />
+
+              <span className="temperature">{weatherData.temperature}</span>
+              <span className="weather-temperature-units"> °C</span>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Feels Like: {weatherData.feels}°</li>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind} km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "1o0f4aea9af54436031a3t4f3b2ca21f";
+    let city = "Toronto";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    let units = "metric";
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
 }
