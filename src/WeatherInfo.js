4f9e1442e1832bea7953ea464b37504dce1d9f8e
haleytoye <hjtoye@gmail.com>
2023-03-09 13:35:40 -0500

    created WI css to maintain edits

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherTemperature from "./WeatherTemperature";
+import "./WeatherInfo.css";
 
 export default function WeatherInfo(props) {
   return (
@@ -13,7 +14,7 @@ export default function WeatherInfo(props) {
         <li className="text-capitalize">{props.data.description}</li>
       </ul>
       <div className="row mt-3">
-        <div className="col-6">
+        <div className="col-7">
           <div className="d-flex">
             <img src={props.data.icon} alt={props.data.icon} />
             <div className="float-left">
@@ -21,7 +22,7 @@ export default function WeatherInfo(props) {
             </div>
           </div>
         </div>
-        <div className="col-6">
+        <div className="col-5">
           <ul>
             <li>Feels Like: {props.data.feels}°</li>
             <li>Humidity: {props.data.humidity}%</li>
